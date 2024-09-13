d13a0266f8814ac1faae1e621d0d25ff5dc5bf30
emrgem <emrgem@bergen.org>
2024-09-13 10:49:57 -0400

    9/13/24 static files tested

--- Unit1_Django_Fundamentals/Lesson3_Urls_Views/sandbox/sandbox/settings.py
+++ Unit1_Django_Fundamentals/Lesson3_Urls_Views/sandbox/sandbox/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'sandbox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Unit1_Django_Fundamentals/Lesson3_Urls_Views/sandbox/sandbox/views.py
+++ Unit1_Django_Fundamentals/Lesson3_Urls_Views/sandbox/sandbox/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello BT! Welcome to HomePage!")
-
+    # return HttpResponse("Hello BT! Welcome to HomePage!")
+    return render(request, 'home.html')
 def about(request):
-    return HttpResponse("All about me!")
+    # return HttpResponse("All about me!")
+    return render(request, 'about.html')
