53d13fe98eb2787850b21809ba63b9bd395e4cf2
emrgem <emrgem@bergen.org>
2024-09-13 10:55:08 -0400

    9/13/24 static files tested

--- Unit1_Django_Fundamentals/Lesson3_Urls_Views/venv/Scripts/activate.bat
+++ Unit1_Django_Fundamentals/Lesson3_Urls_Views/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Google Drive\24-25\_Courses\3-Advanced Programming\code\24-25-PERIOD-3-WEBDEV-CLASSWORK\lesson3\venv
+set VIRTUAL_ENV=D:\Google Drive\24-25\_Courses\3-Advanced Programming\code\24-25-PERIOD-3-WEBDEV-CLASSWORK\Unit1_Django_Fundamentals\Lesson3_Urls_Views\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
