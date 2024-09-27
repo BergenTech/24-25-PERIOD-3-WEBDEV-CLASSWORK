4a50645a15e06850bda299a779e49dfb9cd47a2e
emrgem <emrgem@bergen.org>
2024-09-27 10:00:22 -0400

    9/27/24 Django Templates

--- Unit1_Django_Fundamentals/Lesson7_Django_Templates/myproject/myproject/urls.py
+++ Unit1_Django_Fundamentals/Lesson7_Django_Templates/myproject/myproject/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('about/', views.about),
     path('posts/', include('posts.urls'))
 ]
--- Unit1_Django_Fundamentals/Lesson7_Django_Templates/myproject/posts/views.py
+++ Unit1_Django_Fundamentals/Lesson7_Django_Templates/myproject/posts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def posts_list(request):
-    return render(request,'posts/posts_list.html')
+    context = {
+        'active_link': 'posts'
+    }
+    return render(request,'posts/posts_list.html', context)
