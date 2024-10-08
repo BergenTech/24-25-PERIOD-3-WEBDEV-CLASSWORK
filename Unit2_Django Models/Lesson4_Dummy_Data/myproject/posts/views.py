7d0827cf45c37e012e92c8f73ee91508c5d43142
emrgem <emrgem@bergen.org>
2024-10-08 10:29:42 -0400

    10/8/24 Pages

--- Unit2_Django Models/Lesson4_Dummy_Data/myproject/posts/views.py
+++ Unit2_Django Models/Lesson4_Dummy_Data/myproject/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+# from django.http import HttpResponse
+
 # Create your views here.
 def posts_list(request):
     # posts = Post.objects.all()
@@ -9,3 +11,7 @@ def posts_list(request):
         'posts':posts
     }
     return render(request,'posts/posts_list.html', context)
+def post_page(request, slug):
+    # return HttpResponse(slug)
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html',{'post':post} )
