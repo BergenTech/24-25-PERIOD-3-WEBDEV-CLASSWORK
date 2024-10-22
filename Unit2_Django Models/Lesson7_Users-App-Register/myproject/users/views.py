9e749912e87176d86e31c7a9425432e5216f57db
emrgem <emrgem@bergen.org>
2024-10-22 10:26:48 -0400

    10/22/2024 User Creation Form

--- Unit2_Django Models/Lesson7_Users-App-Register/myproject/users/views.py
+++ Unit2_Django Models/Lesson7_Users-App-Register/myproject/users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
+    #Check if the form is submitted
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) #submitted with data
+        #validate the form
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else: #Get method
+        form = UserCreationForm()
     context = {
         'active_link': 'register',
+        'form':form
     }
     return render(request, 'users/register.html', context)
