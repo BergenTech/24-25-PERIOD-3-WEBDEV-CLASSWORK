8bab3c9635264482531ce43a2e5a557289d4a57a
emrgem <emrgem@bergen.org>
2024-11-19 09:43:06 -0500

    New_Post_Starter

--- Unit2_Django Models/New_Post_Review/myproject/users/forms.py
+++ Unit2_Django Models/New_Post_Review/myproject/users/forms.py
@@ -5,17 +5,18 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-
+    last_name = forms.CharField(max_length=30)
+    
     class Meta:
         model = User
         fields = ("username", "email", "first_name", "last_name", "password1", "password2")
-
+    
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data["email"]
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         if commit:
             user.save()
         return user
+        
--- Unit2_Django Models/New_Post_Review/myproject/users/views.py
+++ Unit2_Django Models/New_Post_Review/myproject/users/views.py
@@ -3,44 +3,39 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login, logout
 
-
 def register(request):
+    #Check if the form is submitted
     if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST) #submitted with data
+        #validate the form
         if form.is_valid():
             login(request, form.save())
             return redirect("posts:list")
-    else:
+    else: #Get method
         form = CustomUserCreationForm()
-
-    # Add Bootstrap classes to form fields
-    for field in form.fields.values():
-        field.widget.attrs["class"] = "form-control"
-
-    context = {"active_link": "register", "form": form}
-
-    return render(request, "users/register.html", context)
-
+    context = {
+        'active_link': 'register',
+        'form':form
+    }
+    return render(request, 'users/register.html', context)
 
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = AuthenticationForm(data = request.POST)
         if form.is_valid():
-            # LOGIN HERE
+            #LOGIN HERE
             login(request, form.get_user())
             return redirect("posts:list")
     else:
         form = AuthenticationForm()
-
-    # Add Bootstrap classes to form fields
+    #Add bootstrap classes to the form field
     for field in form.fields.values():
         field.widget.attrs["class"] = "form-control"
-
-    context = {"active_link": "login", "form": form}
+        
+    context = {"active_link":"login", 'form':form}
     return render(request, "users/login.html", context)
 
 def logout_view(request):
     if request.method == "POST":
         logout(request)
-        return redirect("posts:list")
-    
+        return redirect("users:login")
