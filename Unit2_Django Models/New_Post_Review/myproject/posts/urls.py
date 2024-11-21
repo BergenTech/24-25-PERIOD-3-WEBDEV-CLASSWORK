3d74e205c3da54fac984feab0257abcdf7295c72
emrgem <emrgem@bergen.org>
2024-11-21 10:25:06 -0500

    posts by users

--- Unit2_Django Models/New_Post_Review/myproject/posts/urls.py
+++ Unit2_Django Models/New_Post_Review/myproject/posts/urls.py
@@ -4,6 +4,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name = "list"),
-    path('new-post/', views.new_post, name = "new_post"),
+    path('new-post/', views.new_post, name = "new-post"),
     path('<slug:slug>', views.post_page, name = "page"),
 ]
--- Unit2_Django Models/New_Post_Review/myproject/posts/views.py
+++ Unit2_Django Models/New_Post_Review/myproject/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from . import forms
@@ -22,5 +22,13 @@ def post_page(request, slug):
 
 @login_required(login_url="/users/login")
 def new_post(request):
-    form = forms.CreatePost()
+    if request.method == 'POST': 
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
     return render(request, "posts/new_post.html", {'form':form})
