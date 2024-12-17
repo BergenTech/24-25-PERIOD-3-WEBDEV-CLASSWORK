3a913eaf64992438e2e5b5bc4f764ea0b566825f
emrgem <emrgem@bergen.org>
2024-12-17 10:15:42 -0500

    admin models - listings and realtors

--- Unit3_BTRE/listings/admin.py
+++ Unit3_BTRE/listings/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'dollar_price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 20
+    def dollar_price(self, obj):
+        return f"${obj.price:,}"
+    
+    dollar_price.short_description = 'price'
+
 # Register your models here.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
