cdc6cb5a69905dccd3e760f2785ddb40438eac79
emrgem <emrgem@bergen.org>
2025-01-08 11:23:25 -0500

    listings page updated

--- Unit3_BTRE/listings/views.py
+++ Unit3_BTRE/listings/views.py
@@ -9,8 +9,8 @@ def index(request):
     }
     return render(request, 'listings/listings.html', context)
     
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html', listing_id)
     
 def search(request):
     return render(request, 'listings/search.html')
