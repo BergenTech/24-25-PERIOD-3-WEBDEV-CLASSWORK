7ba255370afd522af54828672f2e6b652b10f3c2
emrgem <emrgem@bergen.org>
2025-01-27 10:24:56 -0500

    search form filtering

--- Unit3_BTRE/listings/views.py
+++ Unit3_BTRE/listings/views.py
@@ -24,9 +24,36 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html',context)
     
 def search(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    # listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    queryset_listings = Listing.objects.order_by('-list_date')
+    # Filter by contains keywords
+    if 'keywords' in request.GET:
+        keywords  = request.GET['keywords']
+        if keywords:
+            queryset_listings = queryset_listings.filter(description__icontains = keywords)
+    # Filter by state
+    if 'state' in request.GET:
+        state  = request.GET['state']
+        if state:
+            queryset_listings = queryset_listings.filter(state__iexact = state)
+    # Filter by city
+    if 'city' in request.GET:
+        city  = request.GET['city']
+        if city:
+            queryset_listings = queryset_listings.filter(city__iexact = city)
+    # Filter by max bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms  = request.GET['bedrooms']
+        if bedrooms:
+            queryset_listings = queryset_listings.filter(bedrooms__lte = bedrooms)
+    # Filter by max price
+    if 'price' in request.GET:
+        price  = request.GET['price']
+        if price:
+            queryset_listings = queryset_listings.filter(price__lte = price)
+        
     context = {
-        'listings': listings,
+        'listings': queryset_listings,
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
