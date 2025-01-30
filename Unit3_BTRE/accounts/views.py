a12ca3ac7216ac0ecd626fa20cac372e9db4216d
emrgem <emrgem@bergen.org>
2025-01-30 10:26:22 -0500

    displaying error message on register page

--- Unit3_BTRE/accounts/views.py
+++ Unit3_BTRE/accounts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
-    if request.get == "POST":
+    if request.method == "POST":
         #Login User
         return
     else:
@@ -12,9 +13,10 @@ def logout(request):
     return render(request, 'accounts/logout.html')
 
 def register(request):
-    if request.get == "POST":
+    if request.method == "POST":
         #Register User
-        return
+        messages.error(request, "Testing Error Messages!")
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
