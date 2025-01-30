ab28bd11a9b2265ae5d68587573fbb72cfbf2e1c
emrgem <emrgem@bergen.org>
2025-01-30 10:09:34 -0500

    messages settings

--- Unit3_BTRE/btre/settings.py
+++ Unit3_BTRE/btre/settings.py
@@ -156,3 +156,10 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
