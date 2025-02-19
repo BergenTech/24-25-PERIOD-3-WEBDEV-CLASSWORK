0178dff2b759e47474971f0deaff3b7741ea6797
emrgem <emrgem@bergen.org>
2025-02-19 09:43:24 -0500

    contact view

--- Unit3_BTRE/contacts/views.py
+++ Unit3_BTRE/contacts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def contact(request):
-    return
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+        
+        # If user is already made an inquiry
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all.filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request,"You already made an inquiry for the listing!" )
+                return redirect('/listings/'+listing_id)
