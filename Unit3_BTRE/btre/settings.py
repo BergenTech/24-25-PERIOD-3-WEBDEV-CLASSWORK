9ea20bb0924c56dff5c3e6df331b64655e09478c
emrgem <emrgem@bergen.org>
2025-02-21 10:23:43 -0500

    smtp gmail

--- Unit3_BTRE/btre/settings.py
+++ Unit3_BTRE/btre/settings.py
@@ -164,3 +164,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'emregemici@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = 'sjbq ihma pvmm tctk'  # Use the App Password generated from Google
--- Unit3_BTRE/contacts/views.py
+++ Unit3_BTRE/contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 def contact(request):
@@ -23,6 +24,14 @@ def contact(request):
                 return redirect('/listings/'+listing_id)
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
+        # Send an email to the ADMIN
+        send_mail(
+            "Property Listing Inquiry",
+            "There has been an inquiry for " + listing + ".Sign into admin panel for more information.",
+            "emregemici@gmail.com", # Your Email
+            ["emrgem@bergen.org"], # Admin Email - Recipient
+            fail_silently=False,
+        )
         messages.success(request, "Your request submitted! A realtor will get back to you soon!")
         return redirect('/listings/'+listing_id)
         
