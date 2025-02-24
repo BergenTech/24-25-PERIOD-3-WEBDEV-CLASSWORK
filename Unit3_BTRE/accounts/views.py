ab67aa18cfe2e714b62feb5cbfa6aa46da842483
emrgem <emrgem@bergen.org>
2025-02-24 10:21:29 -0500

    dashboard view and html updated

--- Unit3_BTRE/accounts/views.py
+++ Unit3_BTRE/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from contacts.models import Contact
 
 # Create your views here.
 def login(request):
@@ -65,4 +66,8 @@ def register(request):
         return render(request, 'accounts/register.html')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_inquiries = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'inquiries': user_inquiries
+    }
+    return render(request, 'accounts/dashboard.html', context)
